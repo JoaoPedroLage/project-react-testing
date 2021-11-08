b4c2df13b281c38a43fbc4fa0f3f048e712a4d98
Joao Pedro de Oli. Lage <JoaoPedro.Lage@outlook.com>
2021-11-08 17:17:37 -0300

    4° Requisite_Test <NotFound.js /> component

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,35 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { NotFound } from '../components';
+
+describe('Test the <NotFound.js /> component', () => {
+  test('Test if the page contains an heading H2 with the text Page requested not found 😭',
+    () => {
+      render(
+        <MemoryRouter>
+          <NotFound />
+        </MemoryRouter>,
+      );
+
+      const headingEl = screen.getByRole('heading', {
+        name: /page requested not found Crying emoji/i,
+      });
+
+      expect(headingEl).toBeDefined();
+    });
+
+  test('Test if the page contains the following picture of a Pokédex',
+    () => {
+      render(
+        <MemoryRouter>
+          <NotFound />
+        </MemoryRouter>,
+      );
+
+      const notFoundImgEl = screen.getByRole('img',
+        { name: /Pikachu crying because the page requested was not found/i });
+
+      expect(notFoundImgEl.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+    });
+});
